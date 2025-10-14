fix attention mask

src/lerobot/policies/smolvla4/modeling_smolvla4.py
@@ -1160,7 +1160,7 @@ class VLAFlowMatching(nn.Module):
 
         if True:
         # with torch.no_grad():
-            past_key_values, prefix_out = self.vlm.prepare_for_generation(
+            past_key_values, prefix_out, hidden_states = self.vlm.prepare_for_generation(
                 attention_mask=attention_matrix_prefix,
                 position_ids=prefix_position_ids,
                 inputs_embeds=prefix_embs,
@@ -1206,7 +1206,7 @@ class VLAFlowMatching(nn.Module):
             prefix_pad_masks, suffix_pad_masks, img_range, lang_range, state_range, box_range, depth_range
         )
 
-        past_key_values, prefix_embs = self.vlm.prepare_for_generation(
+        past_key_values, prefix_embs, hidden_states = self.vlm.prepare_for_generation(
             attention_mask=attention_matrix_prefix,
             position_ids=prefix_position_ids,
             inputs_embeds=prefix_embs,

src/lerobot/policies/smolvla4/smolvlm.py
@@ -172,18 +172,19 @@ class SmolVLM(nn.Module):
             inputs_embeds = inputs_embeds.to(self.vlm_dtype)
             pass
         
+        attention_mask = attention_mask.unsqueeze(1)
         # context attention
         result = self.vlm.forward(
-            attention_mask_cross=None,
-            attention_mask_self=attention_mask,
+            attention_mask=attention_mask,
             position_ids=position_ids,
             inputs_embeds=inputs_embeds,
             use_cache=True,
+            output_hidden_states=False,
         )
         past_key_values = result.past_key_values
         out_embeds = result.last_hidden_state.to(torch.float32)
 
-        return past_key_values.to_legacy_cache(), out_embeds
+        return past_key_values.to_legacy_cache(), out_embeds, result.hidden_states
     
     def forward(
         self,